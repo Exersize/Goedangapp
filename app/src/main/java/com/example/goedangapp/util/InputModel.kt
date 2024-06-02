30e619f82644141ad8d50602fdfa8f4bd44c5aeb
faris <faris.rahmat29@gmail.com>
2024-06-02 15:18:11 +0700

    add data model for input stock

---
+++ app/src/main/java/com/example/goedangapp/util/InputModel.kt
@@ -0,0 +1,10 @@
+package com.example.goedangapp.util
+
+data class InputModel(
+val jenis :String,
+val quantity : Int,
+val iconurl :String,
+val price :Int,
+val total: Int,
+val weight :String,
+)
