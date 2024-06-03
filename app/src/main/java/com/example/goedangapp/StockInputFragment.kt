a2d8c8e74afcfc37a24d0c7c8828a6416ef6785a
BigFatTotoro9 <fadhilahrasyid9@gmail.com>
2024-06-03 23:49:19 +0700

    Finishing StockIn

--- app/src/main/java/com/example/goedangapp/StockInputFragment.kt
+++ app/src/main/java/com/example/goedangapp/StockInputFragment.kt
@@ -1,12 +1,10 @@
 package com.example.goedangapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewParent
-import android.widget.TableLayout
+import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import com.example.goedangapp.databinding.FragmentStockInputBinding
 import com.example.goedangapp.util.PagerAdapter
