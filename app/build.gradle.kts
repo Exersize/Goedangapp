b5a1811e928277904b22bbf9a04df25388bb7c10
BigFatTotoro9 <fadhilahrasyid9@gmail.com>
2024-06-06 14:49:53 +0700

    Revert Google Sign In

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
-    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -51,17 +50,12 @@ dependencies {
     implementation("com.google.android.gms:play-services-auth:21.2.0")
 
 
-    implementation(libs.play.services.auth)
-    implementation(libs.androidx.credentials)
-    implementation(libs.androidx.credentials.play.services.auth)
-    implementation(libs.googleid)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.firebase.auth)
-    testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
--- app/src/main/java/com/example/goedangapp/SignInActivity.kt
+++ app/src/main/java/com/example/goedangapp/SignInActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.credentials.CredentialManager
 import com.example.goedangapp.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
@@ -19,13 +18,8 @@ class SignInActivity : AppCompatActivity() {
         // Placeholder simpel untuk pindah Activity
         // TODO : Integrasi Google Sign In
         binding.signInText.setOnClickListener{
-//            val intent = Intent(this, MainActivity::class.java)
-//            startActivity(intent)
-//            signIn()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
-    }
-
-    private fun signIn() {
-        val credentialManager = CredentialManager.create(this)
     }
 }
